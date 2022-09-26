019bc50c329b3f5096bd401c63d48dc6cf512b3a
SalahSoussou <kaka79702@gmail.com>
2022-09-26 12:47:53 +0100

    ok

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
 <?php
+// comment 
+ echo 'hello word'; // coment
+ #coment
+ echo '<br>';#coment
+//  echo 'hello word';
+//  echo '<br>';#coment
+/*
+coment 1
+coment 2
+coment 3
+*/
+
+echo gettype(true);
+echo '<br> ';
+echo gettype(false);
+echo '<br> ';
+echo gettype(123);
+echo '<br> ';
+echo gettype(12.45);
+echo '<br> ';
+echo gettype('hi ther');
+echo '<br> ';
+echo gettype(array("hu","test"));
+echo '<br> ';
+echo gettype(["hu","test"]);
+echo '<br> ';
+echo '<hr>';
+// =======================================================
 
 echo 'test';
+echo '<br>';
+echo 5 + '5text';
+echo '<br>';
+echo 5 + (int)'5 str';
+
+echo '<br>';
+echo 5 + 7.5;
+echo '<br>';
+echo 5 + (int)7.5;
