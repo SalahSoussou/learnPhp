1d2a077db9b3019ff09bab113167f967a791a58f
SalahSoussou <kaka79702@gmail.com>
2022-09-28 19:27:28 +0100

    ok

--- arr.php
+++ arr.php
@@ -1,6 +1,42 @@
 <?php
 
-echo 'test';
-echo 'test';
-echo 'test';
-echo 'test';
+/*
+= Array
+---------------
+
+*/
+echo '<pre>';
+print_r([
+    'hello',
+    'A' => 'salah',
+    'B' => 'sou',
+    'iddin',
+    "arr" => [12, "ER", "En", 7 => 'test'],
+]);
+echo '<pre>';
+
+//==========================================
+/*
+Variables 
+= Naming Rouls ++
+*/
+
+$userName = 'salah iddine';
+
+echo $userName;
+echo '<br>';
+echo 'Hello $userName';
+echo '<br>';
+echo "Hello $userName";
+echo '<hr>';
+
+//=============
+
+$a = 'var';
+$$a = 'var: 2';
+echo $a;
+echo '<br>';
+echo $$a;
+echo '<br>';
+echo $var;
+echo '<br>';
