8578c9e2dcefdfe01739f9c5a97fe55a6f6a7622
SalahSoussou <kaka79702@gmail.com>
2022-10-01 12:11:40 +0100

    ok

--- operat.php
+++ operat.php
@@ -3,3 +3,30 @@
 
 echo 15  + 20;
 echo '<br>';
+
+$noum = 0;
+$noum++;
+$noum++;
+$noum++;
+$noum++;
+$noum++;
+$noum++;
+echo "num: $noum";
+echo '<br>';
+$noum--;
+$noum--;
+$noum--;
+echo "num: $noum";
+echo '<hr>';
+
+$PN = 'Salah';
+$FN = 'Soussou';
+
+echo "$PN $FN";
+echo '<br>';
+echo "{$PN} {$FN}";
+echo '<br>';
+echo $PN . "-" . $FN;
+
+echo '<br>';
+echo '<br>';
